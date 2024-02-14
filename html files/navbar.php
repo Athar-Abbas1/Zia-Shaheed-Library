578c7c8d317dbe713fae0795b6577a3403167207
Athar Abbas1 <haideraley3@gmail.com>
2024-02-14 15:49:14 +0500

    Created membership form.

---
+++ html files/navbar.php
@@ -0,0 +1,18 @@
+<div class="header">
+    <img class="pic" src=images/dfd2.png alt="" height="100px" width="200px">
+    <div class="link">
+      <div><a href='html files/login.html'>My Account</a></div>
+      <div><a href="html files/donation.html">Donate Us</a></div>
+      <div><a href="html files/about.html">About Us</a></div>
+      <div><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg></div>
+  </div>
+
+  </div>
+   <div class="bar">
+    <div><a href="index.html">Home</a></div>
+    <div><a href="html files/blogs.html" >Blogs</a></div>
+    <div><a href="html files/catalogue.html">Books Catalogue</a></div>
+    <div><a href="html files/membership.html">Membership</a></div>
+    <div><a href="html files/services.html">Services</a></div>
+    <div><a href="html files/staff.html">Staff</a></div>
+     </div>
